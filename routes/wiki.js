fd73d3e8152b824801341649ece2ccdd41eac39f
liadove <alinadavletshina97@gmail.com>
2019-11-14 16:03:23 -0500

    new makeslug function

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,14 +1,37 @@
 const express = require("express");
 const router = express.Router();
 const addPage = require("../views/addPage");
+const { Page } = require ("../models");
 
 router.get("/", async (req, res) => {
   res.send("router get wiki page");
 });
 
-router.post("/", async (req, res) => {
+router.post("/", async (req, res, next) => {
   // console.log(req.body.title, req.body.author);
-  console.log(req.body);
+
+
+  const makeSlug =(title)=>{
+    return title.replace(/\s+/g, `_`).replace(/\W/g, '');
+  }
+
+
+
+  const page = new Page({
+    title: req.body.title,
+    content: req.body.content,
+    status: req.body.status,
+    slug: makeSlug(req.body.title)
+  });
+console.log(page);
+  try{
+    await page.save();
+    res.redirect('/');
+  }
+  catch(error) {next(error)};
+
+
+  // console.log(req.body);
 });
 
 router.get("/add", async (req, res) => {
